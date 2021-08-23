7d5d838515b15a498dca40afb06aabb42b1e2801
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-08-23 15:39:32 -0500

    cat generator challenge javascript workshop

---
+++ assets/javascript.js
@@ -0,0 +1,6 @@
+function generateCat() {
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-cat-gen');
+    image.src = './assets/istockphoto-1161210058-640x640.jpg';
+    div.appendChild(image)
+}
